0cb1f0109ddbce5988c6f59f7c1ece7721814084
Vasily Levit <vasily.levit@gmail.com>
2022-09-20 00:20:16 +0800

    add oneline in task3

--- task3.py
+++ task3.py
@@ -3,5 +3,15 @@
 # 'Мы очень любим Питон'
 
 in_str = 'Мы неабв очень любим Питон иабв Джавабв'
-del_str = 'абв'
+text_find = 'абв'
 in_list = in_str.split()
+# i = 0
+# while i in range(len(in_list)):
+#     if text_find in in_list[i]:
+#         in_list.remove(in_list[i])
+#     else:
+#         i += 1
+# print(in_list)
+
+out_list = [item for item in in_list if text_find not in item]
+print(out_list)
